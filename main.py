12990ade2b57e81bfd082006ee3cfc79053d8d5a
Dankyi Ben-Oni <dankyibenjamin0@gmail.com>
2024-07-04 07:33:17 +0000

    Update to main

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 from flask import Flask, request, jsonify
 import requests
 
+
 app = Flask(__name__)
-
-# Function to get location data from IP
-
-
-def get_location(client_ip):
-    response = requests.get(f'http://ip-api.com/json/{client_ip}')
-    if response.status_code == 200:
-        data = response.json()
-        city = data.get('city', 'Unknown')
-        return city
-    else:
-        return 'Unknown'
-
-# Function to get weather data
 
 
 def get_weather(city):
-    # Replace with your OpenWeatherMap API key
+    #
     api_key = '7a81d3c9c1b11b4cbf1a898da335b1af'
     response = requests.get(
         f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric')
@@ -31,25 +18,40 @@ def get_weather(city):
         return 'Unknown'
 
 
+@app.route('/')
+def home():
+    return "welcome"
+
+
 @app.route('/api/hello', methods=['GET'])
-def hello():
-    visitor_name =  request.args.get('visitor_name')
-    client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    location = get_location(client_ip)
+def api():
+    # getting the vistor name
+    visitor_name = request.args.get('visitor_name')
+    client_ip = request.environ['REMOTE_ADDR']
+    # getting the user ip and location
+    response = requests.get(
+        f'https://ipinfo.io/{client_ip}/json?token=e852942472d06c')
+    data = response.json()
+    location = data.get('city')
+    # client_ip = data.get('ip')
+    # detting the temperature
     temperature = get_weather(location)
-    greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
+    # greeting message
+    greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {location}"
     if visitor_name:
         user_data = {
-            'client_ip': client_ip,
-            'location': location,
-            'greeting': greeting
+            "client_ip": client_ip,
+            "location": location,
+            "greeting": greeting
         }
-        return jsonify(user_data), 200
+        return jsonify(user_data)
     else:
+        # without client name
+        greeting = f"Hello,  the temperature is {temperature} degrees Celcius in {location}"
         user_data = {
-            'client_ip': "null",
-            'location': 'null',
-            'greeting': 'null'
+            "client_ip": client_ip,
+            "location": location,
+            "greeting": greeting
         }
         return jsonify(user_data)
 
